99f11a51e2c3a50a3400ef3114a77c39efeb9bb1
ebpacheco <eduardobertoncinip@live.com>
2025-02-04 20:46:23 -0300

    Operadores aritmeticos

---
+++ aula9.py
@@ -0,0 +1,24 @@
+adicao = 10 + 10
+print('Adicao', adicao)
+
+subtracao = 10 - 5
+print('Subtracao', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicacao', multiplicacao)
+
+# Operador / Realiza a divisão de ponto flutuante, retornando o quociente exato, incluindo a parte decimal
+divisao = 10 / 2.2  # em divisao o retorno é sempre float
+print('Divisao', divisao)
+
+# Operador // Realiza a divisão inteira, retornando o quociente como um número inteiro, descartando a parte decimal
+divisao_inteira1 = 10 // 2
+divisao_inteira2 = 10 // 2.2
+print('Divisao inteira', divisao_inteira1)
+print('Divisao inteira', divisao_inteira2)
+
+exponenciacao = 2 ** 10
+print('Exponenciacao', exponenciacao)
+
+modulo = 55 % 2  # resto da divisao
+print ('Modulo', modulo)
