4de2e5536c8d2a1200b8ce401d5a32ccf4fbc07d
ebpacheco <eduardobertoncinip@live.com>
2025-02-04 20:51:56 -0300

    Concatenacao e repeticao

---
+++ aula10.py
@@ -0,0 +1,9 @@
+# Concatenacao
+concatenacao = 'Eduardo' + ' ' + 'Pacheco'
+print(concatenacao)
+
+# Repeticao
+a_dez_vezes = 'A' * 10
+tres_vezes_eduardo = 3 * 'Eduardo'
+print(a_dez_vezes)
+print(tres_vezes_eduardo)
