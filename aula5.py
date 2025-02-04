07457cb257e65fa21881cbff5d54ec5bd0b4cb5d
ebpacheco <eduardobertoncinip@live.com>
2025-02-04 11:10:23 -0300

    Tipo boolean

---
+++ aula5.py
@@ -0,0 +1,12 @@
+# Tipo de dado bool (boolean)
+# sim(True) ou nao(false)
+
+# Operador == é um operador logico 
+# que verifica se um valor é igual aa outro
+
+print (10 == 10) # sim = True 
+print (10 == 20) # nao = False 
+print (type(True)) # tipo bool
+print (type(False)) # tipo bool
+print (type(10 == 10)) # tipo bool
+print (type(10 == 20)) # tipo bool
