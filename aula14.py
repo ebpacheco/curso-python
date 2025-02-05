e5f4b9bc7308883c0525c53ededa5085f3a655b7
ebpacheco <eduardobertoncinip@live.com>
2025-02-05 16:24:41 -0300

    Refatorando aula 14

--- aula14.py
+++ aula14.py
@@ -2,7 +2,7 @@ a = 'A'
 b = 'B'
 c = 1.1
 
-string = 'a = {}, b = {nome2}, c = {nome3}'
+string = 'a = {}, b = {nome2}, c = {nome3:.2f}'
 formato = string.format(a, nome2 = b, nome3 =c)
 
 print(formato)
