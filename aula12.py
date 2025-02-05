63b0cb3314b6c68b37c8d5fbb0febbbff267cbe9
ebpacheco <eduardobertoncinip@live.com>
2025-02-05 13:54:11 -0300

    Calculo de IMC

---
+++ aula12.py
@@ -0,0 +1,8 @@
+nome = 'Eduardo Pacheco'
+altura = 1.83
+peso = 86
+#imc = peso / (altura * altura)
+imc = peso / altura ** 2
+
+print (nome, 'tem', altura, 'de altura, pesa', 
+       peso, 'quilos e seu IMC é:',imc )
