83d9b2b3f14c3fe398a5a26edcf09f2486cbbec2
ebpacheco <eduardobertoncinip@live.com>
2025-02-07 15:27:32 -0300

    Utilizando os operadores relacionais

---
+++ aula19.py
@@ -0,0 +1,28 @@
+"""
+Operadores de comparacao (relacionais)
+OP          SIGNIFICADO         EXEMPLO(TRUE)
+>           maior               2 > 1
+>=          maior ou igual      2 >= 2
+<           menor               1 < 2
+<=          menor ou igual      2 <= 2
+==          igual               'a' == 'a'
+!=          diferente           'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 > 1
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+print('Maior =', maior)
+print('Maior ou igual = ', maior_ou_igual)
+print('Menor =', menor)
+print('Menor ou igual = ',menor_ou_igual)
+print('Igual =', igual)
+print('Diferente = ', diferente)
+
+"""
+Usar python -i aula19.py no terminal para executar variavel por variavel.
+"""
