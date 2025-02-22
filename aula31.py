c6fc6b8f84936a743971bf8b58567c8433b5ef01
ebpacheco <eduardobertoncinip@live.com>
2025-02-22 10:37:07 -0300

    Identidade do valor na memoria

---
+++ aula31.py
@@ -0,0 +1,13 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Nao valor
+is e is not = é ou nao é (tipo, valor, identidade)
+id = Identidade
+"""
+
+v1 = 'a'
+v2 = 'a'
+v3 = 'b'
+print(id(v1)) #140709284413408
+print(id(v2)) #140709284413408
+print(id(v3)) #140709284413456
