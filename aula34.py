c804bca551997adfab09b26e63141e08d639cc15
ebpacheco <eduardobertoncinip@live.com>
2025-03-14 19:32:56 -0300

    Documentacao do py

---
+++ aula34.py
@@ -0,0 +1,10 @@
+""" https://docs.python.org/pt-br/3.13/
+Tipos imutaveis que vimos: str, int, float, bool"
+""" 
+
+string = 'Eduardo Pacheco'
+# outra_variavel = f'{string[0:3]}ARDO{string[5:]}'
+# print(string)
+# print(outra_variavel)
+# print(outra_variavel.capitalize())
+print(string)
