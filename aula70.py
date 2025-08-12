a5082a9db2327336f91e0d063bdf34a1849464cf
ebpacheco <eduardobertoncinip@live.com>
2025-08-12 16:09:39 -0300

    Valores padrao para parametros em funcoes Python + NoneType

---
+++ aula70.py
@@ -0,0 +1,20 @@
+"""
+Valores padrao para parametros
+Ao definir uma funcao, os parametros podem
+ter valores padrao. Caso o valor nao seja
+enviado para o parametro, o valor padrao sera usado.
+Refatorar: editar o seu codigo
+"""
+
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+
+soma(1,2,3)
+soma(1,2)
+soma(1,2,0)
+soma(z=3, x=1 , y=2)
+
+
