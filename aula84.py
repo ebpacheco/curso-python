48a1ee743affee765e5d121ce6b4301f55af3f70
ebpacheco <eduardobertoncinip@live.com>
2025-10-01 16:21:01 -0300

    Operadores importantes para o tipo set

--- aula84.py
+++ aula84.py
@@ -35,10 +35,27 @@ s1.add(1)
 s1.update((1,2,3,'Pacheco'))
 # s1.clear()
 s1.discard(1)
-print(s1)
+# print(s1)
 
 # Operadores úteis:
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
-# diferença - Itens presentes apenas no set da esquerda
+# diferença "-" - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+
+s1 = {1,2,3}
+s2 = {2,3,4}
+
+s3 = s1 | s2 #união
+print(s3)
+
+s4 = s1 & s2 #intersecção
+print(s4)
+
+s5 = s1 - s2 #diferença apenas no s1
+s6 = s2 - s1 #diferença apenas no s2
+print(s5)
+print(s6)
+
+s7 = s1 ^ s2 #diferença simétrica
+print(s7)
