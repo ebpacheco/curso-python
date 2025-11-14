8220ccde00b5f8b60e52878629d25d506ed18e4a
ebpacheco <eduardobertoncinip@live.com>
2025-11-14 15:19:36 -0300

    Refatorando try e excepct

--- aula102.py
+++ aula102.py
@@ -1,16 +1,19 @@
-# Try, except, else e finally
+# Try e except
 
 try:
     a = 18
     b = 0
     c = a / b
     print(c)
-except ZeroDivisionError:
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
     print('Dividido por zero')
 except NameError:
     print('Nome nao esta definido')
-except (TypeError, IndexError):
+except (TypeError, IndexError) as error:
     print('TypeError + IndexError')
+    print('MSG: ', error)
+    print('Nome: ', error.__class__.__name__)
 except Exception:
     print('ERRO DESCONHECIDO')
 
