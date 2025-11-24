36c1f562e8e4df2b89079daf1d12fa1387eddbfa
ebpacheco <eduardobertoncinip@live.com>
2025-11-24 09:39:08 -0300

    Introducao aos packages

---
+++ aula108.py
@@ -0,0 +1,13 @@
+# import aula108_package.modulo
+# print(aula108_package.modulo.soma_do_modulo(1,2))
+
+
+# from aula108_package.modulo import soma_do_modulo
+# print(soma_do_modulo(1,2))
+
+# from aula108_package import modulo
+# print(modulo.soma_do_modulo(1,2)) 
+
+from aula108_package.modulo import *
+print(soma_do_modulo(1,2))
+print(variavel)
---
+++ aula108_package/modulo.py
@@ -0,0 +1,9 @@
+__all__ = [
+    'variavel',    
+    'soma_do_modulo'
+]
+
+variavel = 'Ola'
+
+def soma_do_modulo(x,y):
+    return x + y
