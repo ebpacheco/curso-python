237edf56ddf0ce336d9f018399f46e7bf59b48b7
ebpacheco <eduardobertoncinip@live.com>
2025-11-28 14:36:01 -0300

    Exercicio unir lista com zip e zip_longest

---
+++ aula116.py
@@ -0,0 +1,26 @@
+# Exercício - Unir listas
+# Crie uma função zipper (como o zipper de roupas)
+# O trabalho dessa função será unir duas
+# listas na ordem.
+# Use todos os valores da menor lista.
+# Ex.:
+# ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+l2 = ['BA', 'SP', 'MG', 'RJ']
+
+def zipper(lista1, lista2):
+    intervalo = min(len(lista1), len(lista2))
+    return [
+        (l1[i], l2[i]) for i in range(intervalo)
+    ]
+
+# print(zipper(l1,l2))
+
+from itertools import zip_longest
+print(list(zip(l1,l2)))
+print()
+print(list(zip_longest(l1,l2, fillvalue='SEM VALOR')))
