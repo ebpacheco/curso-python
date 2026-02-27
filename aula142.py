c9f81dc21ddd1ed4b4b360bf12374cb81bc8a2a0
ebpacheco <eduardobertoncinip@live.com>
2026-02-27 10:43:32 -0300

    Atributos de classe

---
+++ aula142.py
@@ -0,0 +1,17 @@
+class Pessoa:
+    ano_atual = 2026
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+    
+p1 = Pessoa('Eduardo', 33)
+p2 = Pessoa('Natany', 35)
+
+print(Pessoa.ano_atual)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
